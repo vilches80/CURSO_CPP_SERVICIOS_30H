eba47ca3d8f0fc7ca806f459ddc697eed58a15cd
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-03 09:40:36 +0100

    87

--- codigo_DIC_2025/bases_de_datos/mysql/main.cpp
+++ codigo_DIC_2025/bases_de_datos/mysql/main.cpp
@@ -3,17 +3,33 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <mysql/mysql.h>
+#include <string>
+#include <iostream>
+
+#define SERVER "localhost"
+#define USER "antonio"
+#define PASS "antonio"
+#define BD "empresa3"
+#define PORT 3307
+
+void testMySQL() {
+	MYSQL* con;
+
+	// Inicializar la conexion;
+	con = mysql_init(nullptr);
+	if (!mysql_real_connect(con, SERVER, USER, PASS, BD, PORT, nullptr, 0)) {
+		std::cerr << "Error al conectar" << std::endl;
+		return;
+	}
+
+	std::cout << "Conectado a Mysql" << std::endl;
+	mysql_close(con);
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main()
+{
+	testMySQL();
+}
+
