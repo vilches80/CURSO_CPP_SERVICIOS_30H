1bc240b671a11a0b5e4523c147b0fbb2e767f588
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-03 12:31:18 +0100

    87

--- codigo_DIC_2025/crow_json/servicio_empleados/EmpleadoCROW.cpp
+++ codigo_DIC_2025/crow_json/servicio_empleados/EmpleadoCROW.cpp
@@ -13,6 +13,37 @@ EmpleadoCROW::EmpleadoCROW(EmpleadoRepository& repo):repositorio(repo)
 void EmpleadoCROW::run() {
 	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/empleados").methods(crow::HTTPMethod::POST)([this](const crow::request& req) {
+
+		// Recuperamos el objeto json con load para hacer la validacion de la sintaxis:
+		auto body = crow::json::load(req.body);
+		if (!body) {
+			return crow::response(400, "Json incorrecto");
+		}
+
+		try {
+
+			// Para convertirlo directamente a objeto, utiliza nlohmann:
+			json j = json::parse(req.body);
+			//if (j.contains("id"))
+
+			// Para convertirlo a objeto:
+			Empleado emp = j.get<Empleado>();
+
+			// Insertar en la BD:
+			this->repositorio.insertar(emp);
+
+			crow::json::wvalue resul;
+			resul["resultado"] = "Se ha creado el empleado correctamente";
+			return crow::response(201, resul.dump());
+
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, std::string("Error en el servicio: ") + e.what());
+		}
+
+		});
+
 	CROW_ROUTE(app, "/empleados/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
 		try {
 			// Verificar que existe:
@@ -37,7 +68,7 @@ void EmpleadoCROW::run() {
 
 		try {
 			auto empleados = this->repositorio.listarTodos();
-			json j = empleados;
+			json j = empleados; // El vector lo pasa a un array de json, y por cada objeto aplica: to_json
 			return crow::response(j.dump());
 
 		}
