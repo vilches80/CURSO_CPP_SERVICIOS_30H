222edd28e07f3c38498f587087dbe9faf22a883d
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-03 12:41:48 +0100

    12

--- codigo_DIC_2025/crow_json/servicio_empleados/EmpleadoCROW.cpp
+++ codigo_DIC_2025/crow_json/servicio_empleados/EmpleadoCROW.cpp
@@ -13,6 +13,53 @@ EmpleadoCROW::EmpleadoCROW(EmpleadoRepository& repo):repositorio(repo)
 void EmpleadoCROW::run() {
 	crow::SimpleApp app;
 
+
+	CROW_ROUTE(app, "/empleados/<int>").methods(crow::HTTPMethod::PUT)([this](const crow::request& req, int id) {
+
+		// Recuperamos el objeto json con load para hacer la validacion de la sintaxis:
+		auto body = crow::json::load(req.body);
+		if (!body) {
+			return crow::response(400, "Json incorrecto");
+		}
+
+		try {
+
+			// Para convertirlo directamente a objeto, utiliza nlohmann:
+			json j = json::parse(req.body);
+			//if (j.contains("id"))
+
+			// Para convertirlo a objeto:
+			Empleado emp = j.get<Empleado>();
+
+			// Asegurar que el id de la URL y del objeto empleado es el mismo:
+			if (emp.id != id) {
+				return crow::response(400, "El ID del empleado no coincide con el del parametro");
+			}
+
+			// Verificar si el empleado existe en la BD:
+			if (!this->repositorio.recuperarEmpleado(id)) {
+				return crow::response(404, "El ID: "+std::to_string(id) + " del empleado no existe");
+			}
+
+			// Actualizamos los datos del empleado:
+			this->repositorio.actualizar(emp);
+
+			// Respuesta:
+			crow::json::wvalue resul;
+			resul["resultado"] = "Se ha actualizado el empleado correctamente";
+			return crow::response(201, resul.dump());
+
+			// return crow::response(204);  // Todo ok, pero sin contenido
+
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, std::string("Error en el servicio: ") + e.what());
+		}
+
+		});
+
+
+
 	CROW_ROUTE(app, "/empleados").methods(crow::HTTPMethod::POST)([this](const crow::request& req) {
 
 		// Recuperamos el objeto json con load para hacer la validacion de la sintaxis:
