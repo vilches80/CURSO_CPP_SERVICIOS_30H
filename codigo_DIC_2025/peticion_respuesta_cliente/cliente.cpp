6af54877525069bf864effaf16d5f712b8853b54
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-04 09:49:50 +0100

    87

--- codigo_DIC_2025/peticion_respuesta_cliente/cliente.cpp
+++ codigo_DIC_2025/peticion_respuesta_cliente/cliente.cpp
@@ -2,8 +2,39 @@
 //
 
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <zmq.hpp>
+#include <string>
+#include "Persona.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try {
+        // Creamos el contexto indicando el numero de hilos
+        zmq::context_t contexto(1);
+
+        // Creamos el socket seleccionando el tipo de patron a implementar:
+        zmq::socket_t socket(contexto, zmq::socket_type::req);
+        socket.connect("tcp://localhost:5555");
+
+        // Definir el objeto persona:
+        Persona p{ "Juan", 45, true };
+
+        // Convertir el objeto en json y lo enviamos al servidor:
+        nlohmann::json j = p;
+        std::string mensaje = j.dump();
+
+        // Enviamos con send indicando que solo tiene una parte el mensaje:
+        socket.send(zmq::buffer(mensaje), zmq::send_flags::none);
+
+        zmq::message_t response;
+        socket.recv(response, zmq::recv_flags::none);
+
+        std::cout << "Respuesta del servidor: " << response.to_string() << std::endl;           
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+    }
+    return 0;
 }
--- codigo_DIC_2025/peticon_respuesta_server/servidor.cpp
+++ codigo_DIC_2025/peticon_respuesta_server/servidor.cpp
@@ -2,9 +2,38 @@
 //
 
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <zmq.hpp>
+#include "Persona.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Creamos el contexto indicando el numero de hilos
+    zmq::context_t contexto(1);
+
+    // Creamos el socket seleccionando el tipo de patron a implementar:
+    zmq::socket_t socket(contexto, zmq::socket_type::rep);
+
+    // Hace un bind el puerto:
+    socket.bind("tcp://*:5555");
+
+    std::cout << "Servidor zmq rep en el puerto 5555" << std::endl;
+    while (true) {
+        zmq::message_t request;
+
+        // Esperando el mensaje del cliente: SINCRONO -> BLOQUEANTE
+        socket.recv(request, zmq::recv_flags::none); // Es un mensaje de una sola parte
+
+        // Parseamos el mensaje con json:
+        nlohmann::json j = nlohmann::json::parse(request.to_string());
+        Persona p = j.get<Persona>();
+
+        std::cout << "Persona recibida: " << p.nombre << " " << p.edad << std::endl;
+
+        // Enviamos respuesta al cliente:
+        std::string respuesta = request.to_string();
+        socket.send(zmq::buffer(respuesta), zmq::send_flags::none);
+    }
+    return 0;
 }
 
