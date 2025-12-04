1d5a5aad2e0de00afd0e7538a4c539bc64e74a46
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-04 10:36:15 +0100

    87

--- codigo_DIC_2025/comunicaciones_zmq/push_pull_dispatcher/push_pull_dispatcher.cpp
+++ codigo_DIC_2025/comunicaciones_zmq/push_pull_dispatcher/push_pull_dispatcher.cpp
@@ -1,19 +1,25 @@
 // push_pull_dispatcher.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-
 #include <iostream>
+#include <zmq.hpp>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Subcriptor, filtra los mensajes de un topic:
+
+    // Crear el socket y el contexto:
+    zmq::context_t contexto(1);
+    zmq::socket_t socket(contexto, zmq::socket_type::push);
+
+    // Conectar al puerto:
+    socket.bind("tcp://localhost:5557");
+
+    std::cout << "Enviar tareas ...." << std::endl;
+
+    for (int i = 0; i < 10; i++) {
+        socket.send(zmq::buffer("Tarea " + std::to_string((i + 1))), zmq::send_flags::none);
+    }
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
--- codigo_DIC_2025/comunicaciones_zmq/push_pull_worker/push_pull_worker.cpp
+++ codigo_DIC_2025/comunicaciones_zmq/push_pull_worker/push_pull_worker.cpp
@@ -2,18 +2,26 @@
 //
 
 #include <iostream>
+#include <zmq.hpp>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Subcriptor, filtra los mensajes de un topic:
+
+    // Crear el socket y el contexto:
+    zmq::context_t contexto(1);
+    zmq::socket_t socket(contexto, zmq::socket_type::pull);
+
+    // Conectar al puerto:
+    socket.connect("tcp://localhost:5557");
+
+    while (true) {
+        zmq::message_t mensaje;
+
+        socket.recv(mensaje, zmq::recv_flags::none);
+        std::cout << "Recibido: " << mensaje.to_string() << std::endl;
+    }
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
