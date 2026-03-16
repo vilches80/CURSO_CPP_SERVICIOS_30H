fd2aa56bf9bf550c560bd2376e9e7a8430d4d714
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-16 14:59:30 +0100

    87

---
+++ codigo/solucion_docker/solucion_docker/practica3_vcpkg/practica3.cpp
@@ -0,0 +1,13 @@
+﻿// practica3.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "practica3.h"
+#include <fmt/core.h>
+
+using namespace std;
+
+int main()
+{
+	fmt::print("MicroServicio C++ con vcpkg y Docker");
+	return 0;
+}
