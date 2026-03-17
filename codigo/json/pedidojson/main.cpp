864cc7ce4a4fabfc1121630e0ba0eeb473770967
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-17 14:12:11 +0100

    87

--- codigo/json/pedidojson/main.cpp
+++ codigo/json/pedidojson/main.cpp
@@ -5,6 +5,11 @@
 #include <string>
 #include <nlohmann/json.hpp>
 #include <sstream>
+#include <fstream>
+#include <vector>
+
+#include "Pedido.h"
+#include "Utilidades.h"
 
 void testLibreria() {
 	nlohmann::json doc;
@@ -26,12 +31,65 @@ void testLibreria() {
 
 void testPedidos() {
 	std::string s = "10248;WILMK;Federal Shipping;Buchanan;32.38;Finlandia";
+	Pedido p(s);
+
+	nlohmann::json doc = p.to_json();
+	std::cout << doc.dump(4) << std::endl;
 }
+
+void testFicheroCSV_Json() {
+	std::string ficheroCSV = "pedidos.csv";
+	std::string ficheroJSON = "pedidos.json";
+
+	std::ifstream fin(ficheroCSV);
+	std::ofstream fout(ficheroJSON);
+
+	std::string linea;
+	std::vector<std::string> campos;
+	bool cabs = true;
+	std::vector<nlohmann::json> array;
+	nlohmann::json documento;
+
+	if (!fin) {
+		std::cerr << "No se ha encontrado el fichero: " << ficheroCSV << std::endl;
+		return;
+	}
+
+	while (std::getline(fin, linea)) {
+		//std::cout << linea << std::endl;
+
+		if (cabs) {
+			cabs = false;
+
+		}
+		else {
+			Utilidades::split(linea, ';', campos);
+			Pedido p(linea);
+			array.push_back(p.to_json());
+			campos.clear();
+		}
+	}
+
+	// Convertir el array de pedidos a un documento de Json;
+	documento = array;
+
+	// Imprimir en consola:
+	std::cout << documento.dump(4) << std::endl;
+
+	// Imprimir a un fichero: json
+	fout << documento.dump(4) << std::endl;
+	
+	fout.close();
+	fin.close();
+
+}
+
 
 int main()
 {
-	testLibreria();
-	// testPedidos();
+	//testLibreria();
+	//testPedidos();
+	testFicheroCSV_Json();
 	return 0;
 	
 }
