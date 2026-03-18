f75eeb127062ab2d35074cd64335aabf07617605
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-18 13:49:22 +0100

    98

--- codigo/websockets/wscliente/wscliente.cpp
+++ codigo/websockets/wscliente/wscliente.cpp
@@ -20,6 +20,24 @@ void clienteWS() {
         // Definir el contexto de io:
         asio::io_context ioc;
 
+        // Resolver DNS para localhost:
+        tcp::resolver resolver(ioc);
+        auto const results = resolver.resolve("127.0.0.1", "8080");
+
+        // Crear el WS:
+        websocket::stream<tcp::socket> ws(ioc);
+
+        // Conectar al servidor:
+        asio::connect(ws.next_layer(), results.begin(), results.end());
+        
+        // Handshake:
+        ws.handshake("127.0.0.1", "/");
+
+        std::cout << "Conectado al servidor con WebnSockets " << std::endl;
+
+
+
+
     }
     catch (const std::exception& ex) {
         std::cerr << "ERROR: " << ex.what() << std::endl;
