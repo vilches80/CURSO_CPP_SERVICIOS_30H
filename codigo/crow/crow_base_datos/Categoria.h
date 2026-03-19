7cfbe0e4e012f254f45ff435103c8c4da07b0097
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-19 13:08:11 +0100

    87

--- codigo/crow/crow_base_datos/Categoria.h
+++ codigo/crow/crow_base_datos/Categoria.h
@@ -1,8 +1,25 @@
 #pragma once
 
 #include <string>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 struct Categoria {
 	int id;
 	std::string nombre;
 };
+
+// Conversion a JSON:
+void to_json(json& j, const Categoria& c) {
+	j = json{
+		{"id", c.id},
+		{"nombre", c.nombre}
+	};
+}
+
+// Conversion a Categoria:
+void from_json(const json& j, Categoria& c) {
+	j.at("id").get_to(c.id);
+	j.at("nombre").get_to(c.nombre);
+}
